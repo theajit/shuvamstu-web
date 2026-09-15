bfade93e159c034f284af73a795df868247eb3e4
Ajit <ajit@theajit.in>
2026-09-15 18:44:02 +0530

    Fix UI

---
+++ app/data/puja-categories.ts
@@ -0,0 +1,6 @@
+export const pujaCategories = [
+  'Ganapati Puja','Narayana Puja','Bishnu Puja','Salagrama Tulasi Dana','Satyanarayana Puja','Haribansa Saptaha','Gruha Pratistha','Puskra Santi','Sundara Kanda','Nabaratri','Bastu Puja','Gruha Arahmba',
+  'Rudra Pratistha','Siva Puja','Siva Pradosa Puja','Rudrabhisek','Trinatha Mela','Panchanan Mela','Chandi Patha','Bhagabata Patha','Bhababata Jagyan','Gayatri Patha','Hanuman Patha','Dasa Mahabidya Puja',
+  'Sani Mela','Laxmi Puja','Grubara Brata','Sudasa Brata','Baibhaba Laxmi Puja','Durga Puja','Srankranti Homa','Sradha','Barshika Puja','Sai Puja','Santoshi Puja','Gramadebati Homa & Chandi Patha',
+  'Kali Puja','Tarini Mela','Saraswati Puja','Surya Puja','Chandi Puja','Nabagraha Santi','Bagalamukhi Puja','Mandira Pratistha','Kalasrapa Dosa Puja','Nakhyatra Puja','Sarba Debadebi Puja'
+] as const;
